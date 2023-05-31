756bd8a062aa7fe7694a2acb3e3fdfe0779d9d2b
Leonardo Cunha <agile.ice4816@fastmail.com>
2023-05-31 07:28:43 -0300

    lint

--- src/zoom-provider.ts
+++ src/zoom-provider.ts
@@ -39,7 +39,6 @@ function ZoomProvider(this: any, options: ZoomProviderOptions) {
   }, config)
 
 
-
   async function get_info(this: any, _msg: any) {
     return {
       ok: true,
@@ -63,7 +62,7 @@ function ZoomProvider(this: any, options: ZoomProviderOptions) {
               const body = msg.q.properties || {}
 
               const json = await postJSON(makeUrl(meetingUrl), makeConfig({
-                  body
+                body
               }))
 
               return entize(json)
@@ -110,7 +109,6 @@ function ZoomProvider(this: any, options: ZoomProviderOptions) {
   })
 
 
-
   seneca.prepare(async function(this: any) {
     let res =
       await this.post('sys:provider,get:keymap,provider:zoom')
@@ -127,7 +125,7 @@ function ZoomProvider(this: any, options: ZoomProviderOptions) {
 
     const accId = res.keymap.acc_id.value
     const oauthUrl =
-        `oauth/token?grant_type=account_credentials&account_id=${accId}`
+      `oauth/token?grant_type=account_credentials&account_id=${accId}`
     const accCredentials = await postJSON(makeUrl(`${oauthUrl}`), makeConfig())
 
     this.shared.headers.Authorization = `Bearer ${accCredentials.access_token}`
