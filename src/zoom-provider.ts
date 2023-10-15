364a341373c2f69b2d050ac6aa5af79c63349bc7
Leonardo Cunha <agile.ice4816@fastmail.com>
2023-10-15 17:54:15 -0300

    refactor: update load action

--- src/zoom-provider.ts
+++ src/zoom-provider.ts
@@ -60,7 +60,7 @@ function ZoomProvider(this: any, options: ZoomProviderOptions) {
               const meetingId = msg.q.meetingId
 
               const meetingUrl = `v2/meetings/${meetingId}`
-              const json = await getJSON(makeUrl(meetingUrl), makeConfig({}))
+              const json = await getJSON(makeUrl(meetingUrl), makeConfig())
 
               return entize(json)
             }
