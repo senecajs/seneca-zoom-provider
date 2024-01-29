1d644c57f70512c409b228e5fae181a4254181ce
Alex <jk1143298@gmail.com>
2024-01-29 18:47:23 +0100

    live.js

--- test/live.js
+++ test/live.js
@@ -1,6 +1,6 @@
 
 // IMPORTANT: assumes node-fetch@2
-const Fetch = require('node-fetch')
+const Fetch = fetch || require('node-fetch')
 
 const Seneca = require('seneca')
 
@@ -15,93 +15,49 @@ Seneca({ legacy: false })
     // debug: true,
     file: [__dirname + '/local-env.js;?'],
     var: {
-      $TANGOCARD_KEY: String,
-      $TANGOCARD_NAME: String,
-      $TANGOCARD_CUSTID: String,
-      $TANGOCARD_ACCID: String,
+      $ZOOM_CLIENT_SECRET: String,
+      $ZOOM_ACCOUNT_ID: String,
+      $ZOOM_CLIENT_ID: String,
     }
   })
   .use('provider', {
     provider: {
-      tangocard: {
+      zoom: {
         keys: {
-          key: { value: '$TANGOCARD_KEY' },
-          name: { value: '$TANGOCARD_NAME' },
-          cust: { value: '$TANGOCARD_CUSTID' },
-          acc: { value: '$TANGOCARD_ACCID' },
+          client_secret: { value: '$ZOOM_CLIENT_SECRET' },
+          account_id: { value: '$ZOOM_ACCOUNT_ID' },
+          client_id: { value: '$ZOOM_CLIENT_ID' }
         }
       }
     }
   })
   .use('../',{
     fetch: Fetch,
-    entity: {
-      order: {
-        save: {
-          sendEmail: true,
-          sender: {
-            email: 'richard+tangocard.sender.01@ricebridge.com',
-            firstName: 'Sender',
-            lastName: ''
-          }
-        }
-      }
-    }
   })
   .ready(async function() {
     const seneca = this
 
-    console.log(await seneca.post('sys:provider,provider:tangocard,get:info'))
+    console.log(await seneca.post('sys:provider,provider:zoom,get:info'))
     
-    const brands = await seneca.entity("provider/tangocard/brand").list$({
-      country: 'IE', verbose: false
-    })
-    console.log('brands',brands.length)
-    // console.dir(brands,{depth:null})
-    
-    let customers = await seneca.entity("provider/tangocard/customer").list$()
-    console.log('customers', customers.length)
-    console.dir(customers,{depth:null})
-    
-    let orders = await seneca.entity('provider/tangocard/order').list$()
-    console.log('orders',orders.length)
-
-    
-    let mark = Math.random()+''
-    let utid = 'U768452'
-    
-    let order = seneca.entity('provider/tangocard/order').data$({
-      amount: 10,
-      // campaign: 'test01',
-      campaign: '',
-      emailSubject: 'subject '+mark,
-      etid: 'E000000',
-      externalRefID: seneca.util.Nid(),
-      message: 'msg '+mark,
-      notes: 'note '+mark,
-      recipient: {
-        email: 'richard+tangocard.test.01@ricebridge.com',
-        firstName: 'First',
-        lastName: ''
-      },
-      // sendEmail: true,
-      // sender: {
-      //   email: '',
-      //   firstName: '',
-      //   lastName: ''
-      // },
-      utid
+    let meeting = seneca.entity('provider/zoom/meeting').data$({
+      topic: 'My Zoom Meeting',
+      type: 2, 
+      start_time: new Date(),
+      duration: 60
     })
 
     try {
-      order = await order.save$()
-      console.log('order',order)
+      meeting = await meeting.save$()
+      console.log('meeting', meeting)
     }
     catch(e) {
       console.log(e.message)
       console.log(e.status)
       console.log(e.body)
     }
+    
+    const meetings = await seneca.entity("provider/zoom/meeting").list$()
+    console.log('meetings', meetings.length)
 
   })
 
